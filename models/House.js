a7156ac62752598e5e66dc8ff0810971680a764a
Iguta <davidigutas@students.uonbi.ac.ke>
2022-11-01 07:22:58 +0300

    first comit

---
+++ models/House.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const TenantSchema = mongoose.Schema({
+    plotNumber:{type:String, required:true},
+    houseNumber:{type:String, required:true},
+    rate:{type:String, required:true},
+    type:{type:String, required:true},
+    location:{type:String, required:true}
+});
+
+module.exports = mongoose.model("Tenant", TenantSchema);
---
+++ models/Tenant.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const TenantSchema = mongoose.Schema({
+    firstName:{type:String, required:true},
+    lastName:{type:String, required:true},
+    phoneNumber:{type:String, required:true, unique:true},
+    nationalID:{type:Number, required:true,  unique:true},
+    houseNumber:{type:String, required:true}
+});
+
+module.exports = mongoose.model("Tenant", TenantSchema);
